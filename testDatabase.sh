9bd59b0c641705027c1f2431884e6dc4e575cc9b
natalia <natalia@natalia>
2013-05-05 20:18:49 -0400

    Pequenios arreglos

--- testDatabase.sh
+++ testDatabase.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 sudo -u postgres dropdb pmstest
 sudo -u postgres createdb pmstest "Test Environment Database for Project Manager System"
 sudo -u postgres psql -d pmstest -f pms.database.sql
