d86023de53fc560cd3cc782a3db3184097af8226
natalia <natalia@natalia>
2013-05-15 23:30:32 -0400

    pop

---
+++ pms/vista/funcionpop.py
@@ -0,0 +1,17 @@
+import flask.views
+
+def pop13aux():
+    flask.session.pop('aux',None)
+    flask.session.pop('aux1',None)
+    flask.session.pop('aux2',None)
+    flask.session.pop('aux3',None)
+    flask.session.pop('aux4',None)
+    flask.session.pop('aux5',None)
+    flask.session.pop('aux6',None)
+    flask.session.pop('aux7',None)
+    flask.session.pop('aux8',None)
+    flask.session.pop('aux9',None)
+    flask.session.pop('aux10',None)
+    flask.session.pop('aux11',None)
+    flask.session.pop('aux12',None)
+    flask.session.pop('aux13',None)
