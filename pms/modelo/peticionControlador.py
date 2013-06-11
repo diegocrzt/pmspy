454cf0bbfd5e174ae0ee71ba120e65697a7db22e
martinpoletti <martinpoletti@ubuntu.ubuntu-domain>
2013-06-11 10:02:52 -0400

    funciones

--- pms/modelo/peticionControlador.py
+++ pms/modelo/peticionControlador.py
@@ -3,6 +3,7 @@ from initdb import db_session, init_db, shutdown_session
 from pms.modelo.proyectoControlador import getProyectoId
 from pms.modelo.usuarioControlador import getUsuarios
 from pms.modelo.itemControlador import getVersionId, getVersionItem
+from pms.modelo.relacionControlador import calcularCyD, crearGrafoProyecto
 from datetime import datetime
 session = db_session()
 
@@ -279,4 +280,48 @@ def opercionHabilitada(s=None, op=None):
             return s.acciones%10000>=1000
         else:
             return False
+    
+def reiniciarVotacion(ids=None):
+    peticion=getPeticion(ids)
+    peticion.cantVotos=0
+    init_db()
+    session.query(Voto).filter(Voto.peticion_id==ids).delete()
+    session.commit()
+    session.merge(peticion)
+    session.commit()
+    shutdown_session()
+    
+def compararPeticion(ids=None):
+    peticion=getPeticion(ids)
+    aux=peticion.items
+    l=[]
+    for a in aux:
+        l.append(a.id) 
+    r=calcularCyD(l)
+    if r[0]!=peticion.costo or r[1]!=peticion.dificultad:
+        reiniciarVotacion(peticion.id)
+        return True
+    else:
+        return False
+    
+def buscarSolicitud(idv=None):
+    ver=getVersionId(idv)
+    itm= ver.item
+    fase=itm.tipoitem.fase
+    proyecto=fase.proyecto
+    grafo=crearGrafoProyecto(proyecto.id)
+    cola=[]
+    for n in grafo:
+        if int(n.version)==int(idv):
+            n.marca=True
+            cola.append(n)
+    for c in cola:
+        for s in c.entrantes:
+            s.marca=True
+            cola.append(s)
+            if not comprobarItemPeticion(s.version):
+                v=getVersionId(s.version)
+                compararPeticion(v.peticion_id)
+            
+    
     
